9361a2fdaee00d080be5d1b47d71877e02d509c1
Daniel Chistyakov <4i.danila@gmail.com>
2022-03-05 23:23:22 +0300

    Search added

---
+++ search.js
@@ -0,0 +1,40 @@
+var filmsElem = document.getElementById('films');
+var searchInput = document.getElementById('search');
+var searchStatus = document.getElementById('status');
+var debounceTimeout = setTimeout(() => null, 100);
+
+function fetch(method, url, body = {}) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);
+  xhr.responseType = 'json';
+  if (method === 'POST') {
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.send(JSON.stringify(body));
+  } else {
+    xhr.send();
+  }
+  xhr.onload = function () {
+    searchStatus.textContent = '';
+    var data = xhr.response.data;
+    filmsElem.innerHTML = '';
+    for (var i = 0; i < data.length; i++) {
+      let filmElem = document.createElement('li');
+      filmElem.className = 'film';
+      filmElem.innerHTML = `<img class='film-poster' src=${data[i].poster}></img><p>${data[i].title}</p>`;
+      filmsElem.append(filmElem);
+    }
+  };
+
+  xhr.onerror = function () {
+    alert('Запрос не удался');
+  };
+}
+
+searchInput.addEventListener('input', (e) => {
+  clearTimeout(debounceTimeout);
+  debounceTimeout = setTimeout(() => {
+    searchStatus.textContent = 'Загрузка...';
+    filmsElem.innerHTML = '';
+    fetch('POST', 'https://api.dyadka.gq/search', { query: e.target.value });
+  }, 500);
+});
